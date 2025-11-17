1f827018716bf6dbe4a13cdc5a18a72168989a24
Falko Hartmann <136270784+ReofSpa@users.noreply.github.com>
2025-11-17 21:54:06 +0100

    Reduce comment prefix
    Reduce the size of the comment prefix, so the update date is visible better.

--- update.php
+++ update.php
@@ -15,7 +15,7 @@ $sAAAAFB = "Good";
 
 // Check whether a IPv4 address was provided and the A type was defined. Update the DNS entry
 if(isset($_GET["ipv4"]) && !$param["recordAid"]==""){
-	$json = '{"records":[{"value":"'.$_GET["ipv4"].'","comment":"Last updated: '.date('Y-m-d H:i:s', time()).'"}]}';
+	$json = '{"records":[{"value":"'.$_GET["ipv4"].'","comment":"Upd.: '.date('Y-m-d H:i:s', time()).'"}]}';
 	$reply = hetzner_api_query("https://api.hetzner.cloud/v1/zones/".$param["zoneid"]."/rrsets/".$param["recordAid"]."/".$param["recordA"]."/actions/set_records", $param["authid"],"POST",$json);
 	// if there is an error json instead, something went wrong, set to Bad
 	if(isset($reply["error"])){
@@ -25,7 +25,7 @@ if(isset($_GET["ipv4"]) && !$param["recordAid"]==""){
 
 // Check whether a IPv6 address was provided and the AAAA type was defined. Update the DNS entry
 if(isset($_GET["ipv6"]) && !$param["recordAAAAid"]==""){
-	$json = '{"records":[{"value":"'.$_GET["ipv6"].'","comment":"Last updated: '.date('Y-m-d H:i:s', time()).'"}]}';
+	$json = '{"records":[{"value":"'.$_GET["ipv6"].'","comment":"Upd.: '.date('Y-m-d H:i:s', time()).'"}]}';
 	$reply = hetzner_api_query("https://api.hetzner.cloud/v1/zones/".$param["zoneid"]."/rrsets/".$param["recordAAAAid"]."/".$param["recordAAAA"]."/actions/set_records", $param["authid"],"POST",$json);
 	// if there is an error json instead, something went wrong, set to Bad
 	if(isset($reply["error"])){
